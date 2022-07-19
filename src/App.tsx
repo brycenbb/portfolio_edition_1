06b69840225d71371b87737a5224ea89fbd925e6
brycen <brycensoc@gmail.com>
2022-07-19 15:57:54 +0100

    added links to socials

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import './App.css';
-
+import twitter from './assets/twitter.svg';
+import linked from './assets/linkedin.svg';
+import github from './assets/github.svg';
+import github2 from './assets/github2.svg';
 function App() {
   return (
     <div className="App">
@@ -11,7 +14,34 @@ function App() {
           <div className="petalDiag1"></div>
           <div className="petalDiag2"></div>
         </div>
-        <div id="name">{/* B<sup>3</sup> */}</div>
+        <div id="name"></div>
+        <a
+          className="social"
+          target="_blank"
+          rel="noreferrer"
+          href="https://twitter.com/brycensoc"
+        >
+          <img src={twitter} alt="twitter icon"></img>
+          Twitter
+        </a>
+        <a
+          className="social"
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.linkedin.com/in/brycenbb/"
+        >
+          <img src={linked} alt="linkedIn icon"></img>
+          LinkedIn
+        </a>
+        <a
+          className="social"
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/brycenbb"
+        >
+          <img src={github2} alt="Github icon"></img>
+          GitHub
+        </a>
       </div>
       <div></div>
     </div>
